e9ecbca891fbb6d0fa1f7c6550dbbd522b988ba0
ZurabSakalov <sakalovz96@gmail.com>
2023-10-22 02:38:14 +0300

    flake8test

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,11 +1,12 @@
 import os
 from pathlib import Path
-
+from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv()
 
-SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
+SECRET_KEY = os.getenv['SECRET_KEY']
 
 DEBUG = True
 
@@ -36,7 +37,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
 ROOT_URLCONF = 'backend.urls'
 
 TEMPLATES = [
